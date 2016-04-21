867156e4a883336e5317e67dfa8c17cacc30ed89
mikel <mikel@mikel-BM6875-BM6675-BP6375.(none)>
2016-04-21 11:07:42 +0800

    add exist result stdard deviation value calculation

---
+++ result_std_dev_cal.py
@@ -0,0 +1,15 @@
+import sys
+import json
+import numpy as np
+
+if __name__ == '__main__':
+    result_name = sys.argv[1]
+    with open(result_name) as fh:
+        result = json.load(fh)
+    for test_name in result.iterkeys():
+        print "Case name: " + test_name
+        time_list = result[test_name]['time_list']
+        print "Time list: " + str(time_list)
+        print "Std_dev: %f" % np.std(time_list)
+
+        print '\n'
